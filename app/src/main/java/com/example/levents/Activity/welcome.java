6ed5f88af04628974d02f37c7697f6ec66eac48b
Lamthiquynhphuong <lamquynhphuong7@gmail.com>
2024-03-22 10:34:45 +0700

    Phuong commit

---
+++ app/src/main/java/com/example/levents/Activity/welcome.java
@@ -0,0 +1,32 @@
+package com.example.levents.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.levents.R;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        Handler handler= new Handler();
+handler.postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        Intent intent= new Intent(welcome.this, Login_Activity.class);
+startActivity(intent);
+
+    }
+},3000);
+    }
+}
